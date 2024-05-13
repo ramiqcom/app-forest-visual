faa85f493f50226868f5fbd00bdb79d1623a9eb8
Ramadhan <ramiqcom@gmail.com>
2024-05-13 08:33:38 +0200

    check key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import App from '../components/main';
 import inno from '../image/INNO4CFIs.png';
 
 export default async function Home() {
+  console.log(process.env.SERVICE_ACCOUNT_KEY_URL);
+
   return (
     <>
       <App image={inno} />
