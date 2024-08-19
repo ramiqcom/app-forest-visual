0e2daa09849742ee0d51373f441753b05a50a9e3
Ramadhan <ramiqcom@gmail.com>
2024-08-19 08:46:46 +0200

    wait 1000s before load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@ export const dynamic = 'force-dynamic';
 
 import App from '@/components/main';
 import { loadLayersDb, loadLocationsDb, loadPeriodsDb } from '@/module/database';
+import { setTimeout } from 'timers/promises';
 
 export default async function Home() {
+  // Awake the titiler
+  await fetch(`${process.env.TITILER_ENDPOINT}`);
+
+  // Timeout 1 second to wait for the server
+  await setTimeout(1000);
+
   const locations = await loadLocationsDb();
   const location = locations[0];
   const periods = await loadPeriodsDb({ location: location.value as string });
   const period = periods[0];
   const layers = await loadLayersDb({ location: location.value, period: period.value });
   const layer = layers[0];
-
-  // Awake the titiler
-  await fetch(`${process.env.TITILER_ENDPOINT}/healthz`);
 
   return (
     <>
